dc12ab17c5dacafa22451eacb63cb86fd413fe4d
Nandini-KC <nandinikc.xworkz@gmail.com>
2023-07-27 21:22:28 +0530

    Using equals method and comparing two references

--- July26-Encapsulation/src/com/xworkz/encapsulation/app/Dosa.java
+++ July26-Encapsulation/src/com/xworkz/encapsulation/app/Dosa.java
@@ -6,44 +6,53 @@ public class Dosa {
 	private int noOfDosa;
 	private String shopName;
 	private String combination;
-	
+
 	@Override
 	public String toString() {
 		System.out.println("Running toString in Dosa method");
-		return "price= "+this.price +" "+"Type of Dosa= "+this.type +" "+"No of dosa= "+this.noOfDosa +" "+"Shop name= "+this.shopName +" "+"Combination for Dosa= "+this.combination;
-		
+		return "price= " + this.price + " " + "Type of Dosa= " + this.type + " " + "No of dosa= " + this.noOfDosa + " "
+				+ "Shop name= " + this.shopName + " " + "Combination for Dosa= " + this.combination;
+
 	}
-	public int getPrice()
-	{
+
+	public int getPrice() {
 		return price;
 	}
-	public void setPrice(int price)
-	{
-		this.price=price;
+
+	public void setPrice(int price) {
+		this.price = price;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public int getNoOfDosa() {
 		return noOfDosa;
 	}
+
 	public void setNoOfDosa(int noOfDosa) {
 		this.noOfDosa = noOfDosa;
 	}
+
 	public String getShopName() {
 		return shopName;
 	}
+
 	public void setShopName(String shopName) {
 		this.shopName = shopName;
 	}
+
 	public String getCombination() {
 		return combination;
 	}
+
 	public void setCombination(String combination) {
 		this.combination = combination;
 	}
-	
+
 }
